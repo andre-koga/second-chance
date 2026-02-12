cc22f6916f3e38b89754de76a24ceb1b9063984b
Andre Koga <andrehkoga1@gmail.com>
2026-02-11 22:35:42 -0500

    Add initial project settings files for Unity project setup
    - Created ProjectVersion.txt to specify the Unity editor version.
    - Added QualitySettings.asset to define quality settings for different platforms.
    - Introduced SceneTemplateSettings.json for managing scene templates.
    - Created ShaderGraphSettings.asset for shader graph configurations.
    - Added TagManager.asset to manage tags and layers in the project.
    - Introduced TimeManager.asset to configure time settings for the project.
    - Created URPProjectSettings.asset for Universal Render Pipeline settings.
    - Added UnityConnectSettings.asset for Unity services configuration.
    - Introduced VFXManager.asset for visual effects settings.
    - Created VersionControlSettings.asset to manage version control settings.
    - Added XRSettings.asset for XR device settings.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Scalars")]
+    protected Vector2D
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
